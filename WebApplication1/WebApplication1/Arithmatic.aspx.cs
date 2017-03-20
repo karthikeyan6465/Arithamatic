837355dc627ee49351ff07ebcd1c35b97d36bd21
karthikeyan <karthikeyan.ponnusamy@ameexusa.com>
2017-03-20 20:56:06 +0530

    subtract function added

--- WebApplication1/WebApplication1/Arithmatic.aspx.cs
+++ WebApplication1/WebApplication1/Arithmatic.aspx.cs
@@ -18,6 +18,8 @@ namespace WebApplication1
         protected void add_Click(object sender, EventArgs e)
         {
             Response.Write(int.Parse(TextBox1.Text) + int.Parse(TextBox2.Text));
+            Response.Write(int.Parse(TextBox1.Text) - int.Parse(TextBox2.Text));
+
         }
     }
 }
